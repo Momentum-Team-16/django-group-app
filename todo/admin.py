400c6a49e19a7b4109ea954b32c6a8cb65718714
CapelHoworth <capelhoworth@gmail.com>
2023-01-11 10:05:17 -0600

    User, Task, and Completion models made and migrated

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Task, Completion
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Completion)
+
+
+
--- todo/models.py
+++ todo/models.py
@@ -8,3 +8,32 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+
+
+class Task(models.Model):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    IMPORTANCE_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High')
+    ]
+
+    title = models.CharField(max_length=50)
+    details = models.TextField(max_length=500, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    participants = models.ManyToManyField(User, related_name='completed_tasks')
+    checkbox = models.BooleanField(default=False)
+    importance = models.CharField(max_length=1, choices=IMPORTANCE_CHOICES, null=True, blank=True)
+
+
+class Completion(models.Model):
+    completed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='completions')
+    completed_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    debrief = models.TextField(max_length=500, null=True, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='completions')
+
+# class Group(models.Model):
+#     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='tasks')
