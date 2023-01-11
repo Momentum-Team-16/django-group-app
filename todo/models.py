1a95bce9834110eca5d812af0e001290b198ec97
Daniel <danielvillery@gmail.com>
2023-01-11 15:10:14 -0500

    tasks displaying on homepage

--- todo/models.py
+++ todo/models.py
@@ -11,9 +11,9 @@ class User(AbstractUser):
 
 
 class Task(models.Model):
-    LOW = 1
-    MEDIUM = 2
-    HIGH = 3
+    LOW = '1'
+    MEDIUM = '2'
+    HIGH = '3'
     IMPORTANCE_CHOICES = [
         (LOW, 'Low'),
         (MEDIUM, 'Medium'),
@@ -23,17 +23,25 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     details = models.TextField(max_length=500, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
     participants = models.ManyToManyField(User, related_name='completed_tasks')
     checkbox = models.BooleanField(default=False)
-    importance = models.CharField(max_length=1, choices=IMPORTANCE_CHOICES, null=True, blank=True)
+    importance = models.CharField(
+        max_length=1, choices=IMPORTANCE_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} to be completed by {self.owner}'
 
 
 class Completion(models.Model):
-    completed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='completions')
-    completed_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    completed_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='completions')
+    completed_date = models.DateTimeField(
+        auto_now_add=True, blank=True, null=True)
     debrief = models.TextField(max_length=500, null=True, blank=True)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='completions')
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='completions')
 
 # class Group(models.Model):
 #     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='tasks')
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import User, Task, Completion
 
 # Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'todo/index.html', context)
